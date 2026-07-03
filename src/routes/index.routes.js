c6c776e77597b8552c494cb9fd3879b13deafb50
felpsdev03 <111583197+felpsdev03@users.noreply.github.com>
2026-07-02 21:40:59 -0300

    Refactor server initialization and route handling by removing clustering logic and simplifying middleware usage in index.routes.js.

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,9 +4,6 @@ import auth from './auth.routes.js';
 import health from './health.routes.js';
 
 export default server => {
-  server.use((req, res, next) => {
-    health(server, new Router());
-    auth(server, new Router());
-    next();
-  });
+  health(server, new Router());
+  auth(server, new Router());
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,3 @@
-import cluster from 'cluster';
-import os from 'os';
-
 import app from './app.js';
 
-if (cluster.isMaster) {
-  for (let index = 0; index < os.cpus().length; index++) {
-    cluster.fork();
-  }
-} else {
-  app.listen(process.env.PORT || 3333);
-}
+app.listen(process.env.PORT || 3333);
